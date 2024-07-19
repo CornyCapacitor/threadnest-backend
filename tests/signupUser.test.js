f7570b80e5753addd41bac165526803b5ca6e5da
Mateusz Minder <mateusz.minder99@gmail.com>
2024-07-19 19:57:19 +0200

    separated test suites for better code reading

--- tests/signupUser.test.js
+++ tests/signupUser.test.js
@@ -1,70 +1,18 @@
 const request = require('supertest');
 const { expect } = require('chai');
 const app = require('../server');
-const mongoose = require('mongoose');
 const User = require('../models/userModel');
 const bcrypt = require('bcrypt')
-
-describe('GET /api/users/:id', () => {
-  let userId
-
-  // Creating new user before testing
-  before(async () => {
-    // Make sure there's no such user in database
-    await User.deleteMany({ username: 'Test User' })
-
-    const user = new User({
-      username: 'Test User',
-      email: 'testuser@example.com',
-      password: 'password123'
-    })
-    const savedUser = await user.save()
-    userId = savedUser._id
-  })
-
-  // Deleting the user after testing is done
-  after(async () => {
-    await User.findOneAndDelete({ _id: userId })
-  })
-
-  // Tests
-  it('should return user details', async () => {
-    const res = await request(app)
-      .get(`/api/users/${userId}`)
-      .expect(200)
-
-    expect(res.body).to.have.property('_id', userId.toString())
-    expect(res.body).to.have.property('username', 'Test User')
-    expect(res.body).to.have.property('email', 'testuser@example.com')
-  })
-
-  it('should return 404 for wrong id', async () => {
-    const wrongId = '6694153ac85c'
-    const res = await request(app)
-      .get(`/api/users/${wrongId}`)
-
-    expect(res.body).to.have.property('error', 'Invalid user ID')
-  })
-
-  it('should return 404 for non-existing user', async () => {
-    const nonExistingId = new mongoose.Types.ObjectId()
-    const res = await request(app)
-      .get(`/api/users/${nonExistingId}`)
-      .expect(404)
-
-    expect(res.body).to.have.property('error', 'User not found')
-  })
-})
 
 describe('POST /api/users/signup', () => {
   let userId
 
   beforeEach(async () => {
-    // Make sure there's no such user in database
+    // Make sure there's no such user in database before each test
     await User.deleteMany({ username: 'Signup User' })
   })
 
-  // Deleting the user after testing is done
+  // Deleting the user after test is done
   afterEach(async () => {
     await User.deleteMany({ _id: userId })
   })
